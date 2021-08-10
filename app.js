c2f2751e0a11bb895614396158814546b8c4dcda
Sejal Singh <70813068+ssejal942@users.noreply.github.com>
2021-08-10 22:03:29 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+ $("#reset-btn").click(function(){
+    let x = document.getElementById("inputPassword1").value;
+    let y= document.getElementById("inputPassword2").value;
+     if (x === y) {
+      window.open("index2.html");
+    }
+    else if(x!==y){
+      
+    $(".form-text").html("<p>Passwords do not match.</p> ");
+    document.getElementById("inputPassword2").style.border= "3px solid red";
+        }
+    
+ });
+
+
+
+
